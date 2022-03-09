a6c8b38f019c8dc4550c888aa60129c617c5f5a5
VBattis <100841444+VBattis@users.noreply.github.com>
2022-03-09 13:36:38 +0100

    Update bb_mad.py

--- defenses/victim/bb_mad.py
+++ defenses/victim/bb_mad.py
@@ -1,3 +1,7 @@
+""" Taken and modifided from: 
+    Tribhuvanesh Orekondy, https://github.com/tribhuvanesh/prediction-poisoning/blob/master/defenses/victim/bb_mad.py """"
+
+
 import os
 import os.path as osp
 import pickle
